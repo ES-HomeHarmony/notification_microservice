4aed6edcab2f5d05dd4664156084a6e8f1199577
Tiago Gomes <tiagocgomes@ua.pt>
2025-01-03 11:25:03 +0000

    SCRUM-132 | Notification throw Kafka

--- app/main.py
+++ app/main.py
@@ -6,17 +6,30 @@ from email.utils import formataddr
 import smtplib
 from dotenv import load_dotenv
 import os
+from kafka import KafkaConsumer
+import threading
+import json
 
 # Carrega as variáveis de ambiente
 load_dotenv(".env/development.env")
 
 app = FastAPI()
 
+# Kafka consumer setup
+invite_consumer = KafkaConsumer(
+    'invite-request',
+    bootstrap_servers=os.getenv('KAFKA_BOOTSTRAP_SERVERS'),
+    auto_offset_reset='earliest',
+    group_id='email-notifications',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
 # Modelo para requisição de envio de email
 class EmailRequest(BaseModel):
     email_to: EmailStr
     subject: str
     html_message: str
+
 
 # Função para envio de email usando smtplib
 def send_email(email_to: str, subject: str, html_message: str):
@@ -46,9 +59,49 @@ def send_email(email_to: str, subject: str, html_message: str):
         print(f"Erro ao enviar email: {e}")
         raise HTTPException(status_code=500, detail=f"Erro ao enviar email: {e}")
 
-# Endpoint para envio de email
-@app.post("/send-email/")
-async def send_email_endpoint(email: EmailRequest, background_tasks: BackgroundTasks):
-    # Adiciona a tarefa de envio de email em segundo plano
-    background_tasks.add_task(send_email, email.email_to, email.subject, email.html_message)
-    return {"message": "Email enviado para o processamento em segundo plano."}
+def process_invite_messages():
+    print(f"Subscribed to topics: {invite_consumer.subscription()}")
+    for message in invite_consumer:
+        try:
+            invite_data = message.value
+            action = invite_data.get("action")
+            if action == "create_user":
+                user_data = invite_data.get("user_data")
+                if user_data:
+                    process_user_data(user_data)
+        except Exception:
+            pass  # Ignora erros ao processar a mensagem
+
+# Função para lógica de processamento dos dados do usuário
+def process_user_data(user_data):
+    name = user_data.get("name")
+    email = user_data.get("email")
+    subject="Welcome to Home Harmony!"
+    # Personaliza o assunto e o corpo do e-mail
+    template_path = "templates/invite.html"
+
+    # Carrega o conteúdo do arquivo HTML
+    with open(template_path, 'r', encoding='utf-8') as file:
+        html_message = file.read()
+
+    # Substitui as variáveis no template
+    html_message = html_message.replace("{{name}}", "John Doe")
+    send_email(email, subject, html_message)
+
+# Endpoint para envio de email não é necessário mais pois o envio de email é feito na função process_invite_messages está só aqui para teste
+# @app.post("/send-email/")
+# async def send_email_endpoint(email: EmailRequest, background_tasks: BackgroundTasks):
+#     # Adiciona a tarefa de envio de email em segundo plano
+#     background_tasks.add_task(send_email, email.email_to, email.subject, email.html_message)
+#     return {"message": "Email enviado para o processamento em segundo plano."}
+
+
+@app.on_event("startup")
+def startup_event():
+    try:
+        consumer_thread = threading.Thread(target=process_invite_messages, daemon=True)
+        consumer_thread.start()
+        print("Kafka consumer thread started.")       
+        
+    except Exception as e:
+        print(f"Error starting Kafka consumer threads: {e}")
